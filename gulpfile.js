46b1d925ef03a1e8141e9fb584170e3616a87791
Celio Batalha <celio.batalha@gmail.com>
2017-05-15 13:26:33 -0300

    Correcao da atividade Workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,40 @@
 var gulp = require("gulp");
 var del = require("del");
+var htmlmin = require('gulp-htmlmin');
+var sass = require('gulp-sass');
 
-gulp.task("clean-scss", function(){
-    del("dist/css")
+gulp.task("clean", function(){
+    del("./dist")
 });
 
-gulp.task("clean-html", function(){
-    del("dist/index.html")
+gulp.task('move-scss', function () {
+    return gulp
+        .src('./source/scss/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('move-scss',['clean-scss'],function(){
-    return gulp.src('./source/scss/**/*.scss')
-           .pipe(gulp.dest('./dist/css'));
+//gulp.task('move-scss',function(){
+//    return gulp
+//        .src('./source/scss/*.scss')
+//        .pipe(gulp.dest('./dist/css'));
+//});
+
+gulp.task('move-html', function() {
+    return gulp
+        .src('./source/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('move-html',['clean-html'],function(){
-    return gulp.src('./source/**/*.html')
-           .pipe(gulp.dest('./dist/'));
-});
+//gulp.task('move-html',function(){
+//    return gulp
+//        .src('./source/*.html')
+//        .pipe(gulp.dest('./dist'));
+//});
 
 
-gulp.task('background',function(){
-    gulp.watch('./source/**/*.scss'['move-scss']);
-    gulp.watch('./source/**/*.html'['move-html']);
+gulp.task('background',['move-scss','move-html'],function(){
+    gulp.watch('./source/scss/*.scss',['move-scss']);
+    gulp.watch('./source/*.html',['move-html']);
 });
